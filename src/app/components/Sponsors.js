a5017c7c8ff2084bc923c3d89e0607cfd7a1a72e
Courtney Reynoldson <ctreynoldson@gmail.com>
2026-08-18 15:40:24 -0700

    more sponsors

--- src/app/components/Sponsors.js
+++ src/app/components/Sponsors.js
@@ -1,9 +1,69 @@
 import LinkButton from "./LinkButton";
 import Card from "./Card";
 import Image from "next/image";
+
+const logos = [
+    {
+        src: "/glitch_logo.webp",
+        width: 150,
+        height: 150,
+        alt: "Glitch Logo",
+        class: "card--gold",
+    },
+    {
+        src: "/tthe_logo.png",
+        width: 150,
+        height: 150,
+        alt: "TTHE Logo",
+        class: "card--blue",
+    },
+    {
+        src: "/bishops_logo.jpg",
+        width: 150,
+        height: 150,
+        alt: "Bishops Barbershop Logo",
+        class: "card--coral",
+    },
+    {
+        src: "/float_house_logo.png",
+        width: 150,
+        height: 150,
+        alt: "Float House Victoria Logo",
+        class: "card--purp",
+    },
+    {
+        src: "/fired_up_logo.webp",
+        width: 150,
+        height: 150,
+        alt: "Fired Up Logo",
+        class: "card--coral",
+    },
+    {
+        src: "/alphabet_sports_logo.png",
+        width: 150,
+        height: 150,
+        alt: "Alphabet Sports Collective Logo",
+        class: "card--rose",
+    },
+    {
+        src: "/the_old_hat.png",
+        width: 150,
+        height: 150,
+        alt: "The Old Hat Logo",
+        class: "card--gold",
+    },
+    {
+        src: "/butterfly_gardens_logo.png",
+        width: 150,
+        height: 150,
+        alt: "Butterfly Gardens Logo",
+        class: "card--purp",
+    },
+];
+
 export default () => (
     <section className="w-full p-6">
-        <h2 className="text-center">Thank you to our sponsors!</h2>
+        <h2 className="text-center">Thank you to our sponsors and supporters!</h2>
         <div className="flex flex-col flex-wrap gap-8 items-center lg:flex-row lg:justify-center">
             <Card cardClass="card--image card--purp h-32">
                 <Image
@@ -29,6 +89,16 @@ export default () => (
                     alt="Malahat Skywalk Logo"
                 />
             </Card>
+            {logos.map((logo, index) => (
+                <Card cardClass={`card--image ${logo.class}`} key={index}>
+                    <Image
+                        src={logo.src}
+                        width={logo.width}
+                        height={logo.height}
+                        alt={logo.alt}
+                    />
+                </Card>
+            ))}
             <Card cardClass="card--gold text-center">
                 <h2>Want to become a sponsor?</h2>
                 <LinkButton
